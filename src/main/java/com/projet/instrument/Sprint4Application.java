fa182ec3a4da731aba158f0c0a771962cacf0772
yzbaya <101043195+yzbaya@users.noreply.github.com>
2023-05-09 23:56:41 +0100

    Initial commit

---
+++ src/main/java/com/projet/instrument/Sprint4Application.java
@@ -0,0 +1,13 @@
+package com.projet.instrument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint4Application.class, args);
+	}
+
+}
